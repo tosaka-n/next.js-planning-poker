f0d23a9540d8a7903ac1907bf04360e092d53732
tosaka-n <tosquitoz@gmail.com>
2022-05-02 13:32:47 +0900

    feat: show message default 5 && set userId, add clear button

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -2,43 +2,78 @@ import { Box, Button, Container, Input, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { Layout } from "src/components/Layout";
 import io from "socket.io-client";
+import { nanoid } from "nanoid";
 
 const Home = () => {
   const [socket, _] = useState(() => io());
-  const [messages, setMessages] = useState<any[]>([]);
-  const [message, setSendMessages] = useState<string>();
+  const [messageLogs, setMessageLogs] = useState<
+    { message: string; userId: string }[]
+  >([]);
+  const [messageValue, setSendMessages] = useState<string>();
+  const [userId, setUserId] = useState<string>();
+  const [messageLength, setShowMessageLength] = useState<number>(5);
   const roomId = 100;
 
   useEffect(() => {
+    let savedUserId = localStorage.getItem("userId");
+    if (!savedUserId) {
+      savedUserId = nanoid(5);
+      localStorage.setItem("userId", savedUserId);
+    }
+    setUserId(savedUserId);
     if (!roomId) {
       return;
     }
+    socket.on("roomInfo", (data: { message: string; userId: string }[]) => {
+      if (data.length === 0) {
+        setMessageLogs([]);
+      }
+      setMessageLogs((prev) => [...prev, ...data]);
+    });
     socket.on("connect", () => {
       console.log("join");
-      socket.emit("join", roomId);
+      socket.emit("join", { roomId, userId: savedUserId });
     });
-    socket.on("message", (data) => {
-      setMessages((prev) => [...prev, { message: data.message }]);
-      console.log(data);
+    socket.on("message", (data: { message: string; userId: string }) => {
+      setMessageLogs((prev) => [...prev, data]);
     });
   }, [roomId]);
   const sendMessage = (event: React.MouseEvent<HTMLButtonElement>): void => {
-    socket.emit("message", { message: message, roomId });
+    socket.emit("message", { message: messageValue, roomId, userId });
+    setSendMessages("");
+  };
+  const clearAllMessage = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ): void => {
+    socket.emit("clearAll", { roomId });
+    setMessageLogs([]);
   };
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSendMessages(event.currentTarget.value);
   };
+  const handleReadMore = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    setShowMessageLength(messageLength + 5)
+  }
+  const handleReadLess = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    setShowMessageLength(messageLength - 5)
+  }
+
   return (
     <Layout>
       <Container>
-        <Box>
-          {messages.map((v) => v.message).join("\n")}
-          {messages.map((msgs, index) => (
-            <Text key={index}>{msgs.message}</Text>
-          ))}
-        </Box>
-        <Input onChange={handleChange}></Input>
+        <Input value={messageValue} onChange={handleChange}></Input>
         <Button onClick={sendMessage}>Send</Button>
+        <Button onClick={clearAllMessage}>Clear All</Button>
+        <Box>
+          {messageLogs
+            .slice(-messageLength)
+            .reverse()
+            .map((msgs, index) => (
+              <Text key={index}>{msgs.userId}: {msgs.message}</Text>
+            ))}
+        </Box>
+        <Button disabled={messageLength > messageLogs.length} onClick={handleReadMore}>Read More</Button>
+        <Button disabled={messageLength <= 5} onClick={handleReadLess}>Less</Button>
       </Container>
     </Layout>
   );
