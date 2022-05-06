f68f72667f1f0e3e4cf025ea8a1ded93eb1e0196
tosaka-n <tosquitoz@gmail.com>
2022-05-06 12:43:58 +0900

    add: link to top on head

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,32 @@
-import { Box, HStack, Image, Spacer } from "@chakra-ui/react";
+import { Box, HStack, Image, Link, Spacer } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
-export const Header = () => (
-  <Box
-    position={'sticky'}
-    top={0}
-    zIndex={2}
-    maxW={'100vw'}
-    bgColor={'white'}
-    fontSize={"2rem"}
-    h={'3rem'}
-    borderBottom={'solid 5px'}
-    borderColor={'red'}
-  >
-    <HStack spacing={0}>
-      <Box
-        pl={'1rem'}
-      >
-        {'HEAD TITLE'}
-      </Box>
-      <Spacer/>
-      <Box pr={'1rem'}>
-        <Image h={'2rem'} src='https://xsgames.co/randomusers/avatar.php?g=male' />
-      </Box>
-    </HStack>
-  </Box>
-)
+export const Header = () => {
+  const router = useRouter();
+
+  return (
+    <Box
+      position={"sticky"}
+      top={0}
+      zIndex={2}
+      bgColor={"white"}
+      fontSize={"2rem"}
+      h={"3rem"}
+      w={"100vw"}
+      borderBottom={"solid 5px"}
+      borderColor={"red"}
+    >
+      <HStack spacing={0}>
+        <Box
+          pl={"1rem"}
+          as={Link}
+          onClick={() => router.push("/")}
+          style={{ textDecoration: "none" }}
+        >
+          {"HEAD TITLE"}
+        </Box>
+        <Spacer />
+      </HStack>
+    </Box>
+  );
+};
